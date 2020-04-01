4c5f9c11c8b2ecad529cffe0273a4e0c02185e29
qq1134993111 <1134993111@qq.com>
2020-04-01 23:50:28 +0800

    添加新编码

--- README.md
+++
@@ -1,2 +0,0 @@
-# conversion_gbk_utf8
-C++11 gbk和utf8编码的相互转换
--- conversion_gbk_utf8/conversion.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<string>
-
-std::string utf8_to_gbk(const std::string & utf8);
-std::string gbk_to_utf8(const std::string & gbk);
