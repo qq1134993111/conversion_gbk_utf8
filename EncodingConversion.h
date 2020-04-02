1c63961404e38683d1dde07064ba5998781cc24c
qq1134993111 <haoyuan_forever@hotmail.com>
2020-04-02 13:21:53 +0800

    private

--- EncodingConversion.h
+++ EncodingConversion.h
@@ -13,10 +13,6 @@ public:
 
 	static std::string    ToString(const std::wstring& wstr);
 	static std::wstring   ToWString(const std::string& str);
-	static std::string    ToGBK(const std::wstring& wstr);
-	static std::wstring   FromGBK(const std::string& str);
-	static std::string    ToUTF8(const std::wstring& wstr);
-	static std::wstring   FromUTF8(const std::string& str);
 	static std::string    GBKToUTF8(const std::string& str);
 	static std::string    UTF8ToGBK(const std::string& str);
 	static std::u16string UTF8toUTF16(const std::string& str);
@@ -25,6 +21,11 @@ public:
 	static std::u32string UTF16toUTF32(const std::u16string& str);
 	static std::string    UTF32toUTF8(const std::u32string& str);
 	static std::u16string UTF32toUTF16(const std::u32string& str);
+private:
+	static std::string    ToGBK(const std::wstring& wstr);
+	static std::wstring   FromGBK(const std::string& str);
+	static std::string    ToUTF8(const std::wstring& wstr);
+	static std::wstring   FromUTF8(const std::string& str);
 private:
 	static const char* GBK_LOCALE_NAME;
 };
